a9eb14861af1bf93317950ff1b32cfab19cb7c59
apoorva9718 <79907306+apoorva9718@users.noreply.github.com>
2021-05-19 15:53:47 +0530

    Add files via upload

--- src/app.py
+++ src/app.py
@@ -1,8 +1,12 @@
+#src/app.py
 from flask import Flask
-from .models import db, bcrypt
+from flask import Flask
 from .config import app_config
+from .models import db, bcrypt
 
-from .views.UserView import user_api as user_blueprint # add this line
+# import user_api blueprint
+from .views.UserView import user_api as user_blueprint
+from .views.BlogpostView import blogpost_api as blogpost_blueprint
 
 def create_app(env_name):
   """
@@ -14,17 +18,19 @@ def create_app(env_name):
 
   app.config.from_object(app_config['development'])  #env_name
 
-  bcrypt.init_app(app) # add this line
-
-  db.init_app(app) # add this line
+  # initializing bcrypt and db
+  bcrypt.init_app(app)
+  db.init_app(app)
 
   app.register_blueprint(user_blueprint, url_prefix='/api/v1/users')
+  app.register_blueprint(blogpost_blueprint, url_prefix='/api/v1/blogposts')
 
   @app.route('/', methods=['GET'])
   def index():
     """
     example endpoint
     """
-    return 'Congratulations! Your first endpoint is working'
+    return 'Congratulations! Your part 2 endpoint is working'
 
   return app
+
